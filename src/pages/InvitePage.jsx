3d744cc5ccff9385d6c5b69d256907b018560492
MackG7 <mackenson_gustave0409@hotmail.com>
2025-11-13 23:11:24 -0300

    error done

--- src/pages/InvitePage.jsx
+++ src/pages/InvitePage.jsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import api from "../api/axios";
-import "../styles/Auth.css";
+import "../styles/global.css";
 
 export default function InvitePage(){
     const [inviteLink, setInviteLink] = useState("");
